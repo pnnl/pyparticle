d8f4b1085574c3060ed07781c4e5908e6483efe0
laura.fierce@pnnl.gov <laura.fierce@pnnl.gov>
2025-06-11 14:24:37 -0700

    updated optics/__init__.py

--- PyParticle/optics/__init__.py
+++ PyParticle/optics/__init__.py
@@ -2,6 +2,7 @@ from .factory import create_optical_particle, MORPHOLOGY_REGISTRY
 from .base import OpticalParticle
 from .utils import get_cross_section_array_from_population, OPTICS_TYPE_MAP
 from .population import OpticalPopulation
+from .refractive_index import RefractiveIndex, RI_fun
 
 def make_optical_particle(
     particle,
