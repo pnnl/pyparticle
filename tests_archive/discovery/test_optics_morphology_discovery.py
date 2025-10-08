b0536bf3daaedac5ac28d512a29badc4e8be5d71
laura.fierce@pnnl.gov <laura.fierce@pnnl.gov>
2025-10-07 21:36:59 -0700

    updated tests

--- tests_archive/discovery/test_optics_morphology_discovery.py
+++
@@ -1,8 +0,0 @@
-from PyParticle.optics import factory as oreg
-
-def test_homogeneous_available():
-    types = oreg.registry._morphology_registry if hasattr(oreg, 'registry') else oreg.discover_morphology_types()
-    # Prefer the public discover function
-    if callable(getattr(oreg, 'discover_morphology_types', None)):
-        types = oreg.discover_morphology_types()
-    assert "homogeneous" in types
