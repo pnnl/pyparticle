2c00d8c44a1d00261caee2638b70166090cf8394
laura.fierce@pnnl.gov <laura.fierce@pnnl.gov>
2025-10-15 16:41:14 -0700

    removing src/yourpkg -- not sure where that came from

--- src/yourpkg/__init__.py
+++
@@ -1,8 +0,0 @@
-# >>> RELEASE KIT START
-"""yourpkg – short description."""
-from importlib.metadata import version, PackageNotFoundError
-try:
-    __version__ = version("yourpkg")
-except PackageNotFoundError:
-    __version__ = "0.0.0"
-# <<< RELEASE KIT END
